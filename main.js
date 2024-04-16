e55774480d98f5361c980e2cc7d5740f3d129dbc
YuriAmani <yuriamani7@gmail.com>
2024-04-16 08:14:30 +0300

    adding-event-listeners

--- main.js
+++ main.js
@@ -1,110 +1,110 @@
-document.addEventListener("DOMContentLoaded", (e) => {
-    e.preventDefault()
-    fetchData()
-    //basketContent()
-})
+document.addEventListener("DOMContentLoaded", () => {
+    fetchData();
+});
 
-let basket =  JSON.parse(localStorage.getItem("data")) || [] 
-console.log(basket);
-
-// function basketContent() {
-//     fetch("http://localhost:3000/basket")
-//     .then((res) => res.json())
-//     .then(basket => {console.log(basket);})
-// }
-
-//const basket = basketContent()
+let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 function fetchData() {
     fetch("http://localhost:3000/gadgets")
-    .then((res) => res.json())
-    .then(generateShop)
+        .then((res) => res.json())
+        .then(generateShop);
 }
 
+function generateShop(shopItemsData) {
+    shop.innerHTML = shopItemsData
+        .map((x) => {
+            let { id, name, price, color, capacity, img } = x;
+            localStorage.setItem('data', JSON.stringify(basket));
 
-        
-function generateShop(shopItemsData){
-            return (shop.innerHTML= shopItemsData.map((x) => {
-                let {id,name,price,color,capacity,img} = x
-                localStorage.setItem('data', JSON.stringify(basket)) //post
-                let search = basket.find((x) => x.id === id) || []
-                
-                return `
+            let search = basket.find((x) => x.id === id) || [];
+
+            return `
                 <div id= product-id-${id} class="item">
-                <img width="220" height="300" src=${img} alt="">
-                <div class="details">
-                    <h3>${name}</h3>
-                    <p>${color}</p>
-                    <p>${capacity}</P>
-                    <div class="price-quantity">
-                        <h2>$ ${price}</h2>
-                        <div class="buttons">
-                            <i onclick='decrement(${id})'  class="bi bi-dash-lg"></i>
+                    <img width="220" height="300" src=${img} alt="">
+                    <div class="details">
+                        <h3>${name}</h3>
+                        <p>${color}</p>
+                        <p>${capacity}</P>
+                        <div class="price-quantity">
+                            <h2>$ ${price}</h2>
                             <div id='${id}' class="male">${search.item === undefined ? 0 : search.item}</div>
-                            <i onclick='increment(${id})'  class="bi bi-plus-lg"></i>
-                            </div>
+                            <button class="increment-button" data-id="${id}">+</button>
+                            <button class="decrement-button" data-id="${id}">-</button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            </div>
-            `
-            }).join(""))
+            `;
+        })
+        .join("");
 
-        }
+    addEventListenersToButtons();
+}
 
+function addEventListenersToButtons() {
+    const incrementButtons = document.querySelectorAll('.increment-button');
+    const decrementButtons = document.querySelectorAll('.decrement-button');
 
-   
+    incrementButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const itemId = button.getAttribute('data-id');
+            increment(itemId);
+        });
+    });
+
+    decrementButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const itemId = button.getAttribute('data-id');
+            decrement(itemId);
+        });
+    });
+}
+
 let increment = (id) => {
     console.log('increment is working');
-    let selecteditem = id
-    let search = basket.find((x) => x.id === selecteditem.id)
+    let selecteditem = id;
+    let search = basket.find((x) => x.id === selecteditem);
     console.log(search);
     if (search === undefined) {
         basket.push({
-            id: selecteditem.id,
+            id: selecteditem,
             item: 1
-        })} 
-        else {
-         search.item += 1
-     }
-     update(selecteditem.id)
-    console.log(selecteditem.id);
-    
+        });
+    } else {
+        search.item += 1;
+    }
+    update(selecteditem);
+    console.log(selecteditem);
+
     console.log(basket);
-    localStorage.setItem('data', JSON.stringify(basket)) 
-    
+    localStorage.setItem('data', JSON.stringify(basket));
 }
-
-
 
 let decrement = (id) => {
     console.log('decrement is working');
-    let selecteditem = id
-    let search = basket.find((x) => x.id === selecteditem.id)
-    if (search === undefined) return
+    let selecteditem = id;
+    let search = basket.find((x) => x.id === selecteditem);
+    if (search === undefined) return;
     else if (search.item === 0) {
-        return }else {
-        search.item -= 1
+        return;
+    } else {
+        search.item -= 1;
     }
-    update(selecteditem.id)
+    update(selecteditem);
 
-     basket = basket.filter((x) => x.item !== 0)
-     localStorage.setItem('data', JSON.stringify(basket)) //post--patch
-    
- }
-
-
- let update = (id)=> {
-     let search = basket.find((x) => x.id === id)
-    document.getElementById(id).innerHTML = search.item
-    
-    calculation()
+    basket = basket.filter((x) => x.item !== 0);
+    localStorage.setItem('data', JSON.stringify(basket));
 }
 
+let update = (id) => {
+    let search = basket.find((x) => x.id === id);
+    document.getElementById(id).innerHTML = search.item;
 
+    calculation();
+}
 
 let calculation = () => {
-    let catIcon = document.getElementById('cartAmount')
-    catIcon.innerHTML = basket.map((x) => x.item).reduce((x,y) => x+y, 0)
+    let catIcon = document.getElementById('cartAmount');
+    catIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
 }
 
- calculation()
+calculation();
